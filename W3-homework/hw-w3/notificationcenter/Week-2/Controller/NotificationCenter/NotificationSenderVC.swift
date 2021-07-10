6c2764c6b23813c3b0aa5bdebe469f0a0b13a9b0
acoban96 <51994626+acoban96@users.noreply.github.com>
2021-07-11 00:54:16 +0300

    Added notification center hw

---
+++ W3-homework/hw-w3/notificationcenter/Week-2/Controller/NotificationCenter/NotificationSenderVC.swift
@@ -0,0 +1,33 @@
+//
+//  NotificationSenderVC.swift
+//  Week-2
+//
+//  Created by Kerim Caglar on 27.06.2021.
+//
+
+import UIKit
+
+class NotificationSenderVC: UIViewController {
+
+    var notificationData: [String: Any] = [:]
+    
+    @IBOutlet weak var notificationTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    //MARK: Gönderilecek Data Kullanıcıdan Alınsın...
+    //MARK: frame vs bound farkı nedir? Açıklayınız..
+
+    @IBAction func sendNotificationBtn(_ sender: Any) {
+        
+        notificationData["name"] = notificationTextField.text
+        
+        NotificationCenter.default.post(name: .sendDataNotification, object: nil, userInfo: notificationData)
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
